ec8bed453ac9ba09f25804c39b8f4b0ea80cb870
Tanya Butenko <tanya@butenko.me>
2016-09-07 23:02:47 +1000

    Update basic.js

--- js/basic.js
+++ js/basic.js
@@ -101,21 +101,98 @@ console.log(multipleOfNumbers);
     };
     So, our function will take 1 argument and return argument multiplied by 2. But for now it will do nothing as we need to call for our function.
     To call the function we do so:
-    var number = 10;
-    functionName(number);
-    Now we will call our function with argument equal to number variable that is 10. And our function will return us 20. To see what our function
+    functionName(10);
+    Now we will call our function with argument that is 10. And our function will return us 20. To see what our function
     returns us we can console.log it, for example.
  */
 
-//TODO: It's your turn to create a function. Step 1 - Name it addition and pass in two argumnets (num1 and num2). This function should return us a summ of num1 and num2. To see result you can alert it or console.log it - to be sure that it works right.
+//TODO: It's your turn to create a function. 
+//Step 1 - Name it add and pass in two argumnets (num1 and num2). 
+//Step 2 - This function should return us a summ of num1 and num2. 
+//Step 3 - Call the function with numbers 2 and 3. To see result you can alert it or console.log it - to be sure that it works right.
 
-var addition = function(num1, num2){
+var add = function(num1, num2){
     return num1 + num2;
 };
 
+console.log(add(2, 3));
+
+
+//TODO: Great, you made it! Now let's do another function named substract and pass 2 arguments num1 and num2.
+//Call on it with numbers 5 and 1 and console.log the result.
+//PS: do you know that instead of numbers you can create variables that store those numbers and pass them as an arguments to your function, try it up.
+
+var firstNum = 5;
+var secondNum = 1;
+
+var substract = function(num1, num2){
+    return num1 - num2;
+};
+
+console.log(substract(firstNum, secondNum));
+
+
+/*If-else statements
+    What if we want our program to make a decision which function it should run? In this case we can use conditions.
+    If spider is big, action should be - run and save your life, but if it is tiny you can simply walk away.
+    Same with code, you can give 'if' condition to machine to make a decision what part of code to run.
+    Structure:
+    if(condition){
+        do this
+    } else {
+        do something else
+    }
+    Example:
+    var number = 7;
+    if (number > 7) {
+        console.log('Our number is bigger then 7');
+    } else {
+        console.log('Our number is smaller then 7');
+    };
+*/
+
+/*Comparison operators
+    You remember we were talking about arithmetical operators and that we have different operators in programming, so
+    here comes time to introduce you comparison operators that we can use to compare two values(>, <, <=, =>, ==, !=).
+    Most of them you know from math classes in school, some of them can be new for you, so '==' is checking equality, if two values are equal.
+    '!=' - checks if they are not equal.
+    PS: Don't mix up '=' and '==' as they have different meaning.
+*/
+
+//TODO: So now we have 2 functions from previous task - add and substract. Let's tell machine to decide what action to run
+//depending on arithmetical operator(+,-,/, * etc). If operator is '+' we should use add function, else - substract function.
+//Step 1, create a variable called operator and let it be equal to '-'
+//Step 2, create if/else statement, pass any 2 arguments to your functions.
+//Don't forget to console.log it to see the result
+
+
+if(operator == '+'){
+    console.log(add(10, 6));
+} else {
+    console.log(substract(4, 1));
+};
+
+/*If-else if-else
+    Hm, but what if we have 4 arithmetical operations in our calculator? Well, we can use if - else if - else structure.
+    Example:
+    var number = 7;
+    if (number > 7) {
+        console.log('Our number is bigger then 7');
+    } else if (number < 7){
+        console.log('Our number is smaller then 7');
+    } else {
+        console.log('Our number is equal to 7');
+    };
+*/
+
+
+//TODO: Let's creat 2 more functions and name them divide and multiply. After that let's extand our if else check and see if 
+// our operator is equal to '+' call add function, else if operator is equal to '-' call substract function, if it equal to 
+// '/' call divide function, if it is '*' call multiply function, else console.log - Sorry, we don't know this operator.
 
 
 
-//if/else
+
+
 
 
