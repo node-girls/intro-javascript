3c2d4d81af825597bde867ee1c4f2fbee61e7521
Tanya Butenko <Tanya.Butenko@tabcorp.com.au>
2016-09-08 14:59:54 +1000

    dom manipulation code

--- js/dom-manipulation.js
+++ js/dom-manipulation.js
@@ -1,1 +1,19 @@
 //getting and adding elements to the page (pre-written code with explanations)
+
+window.onload = function () {
+
+    var getElement = function (classname) {
+        return document.querySelector(classname);
+    };
+
+    var getValue = function (classname) {
+        var el = getElement(classname);
+        return el.innerHTML;
+    };
+
+    var displayValue = function (classname, value) {
+        var el = getElement(classname);
+        return el.innerHTML = value;
+    };
+
+};
