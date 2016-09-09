9bcf70c2ee3ada5257512daa11e5343d87c822b1
Tanya Butenko <tanya@butenko.me>
2016-09-09 21:38:07 +1000

    arrays and loops

--- js/dom-manipulation.js
+++ js/dom-manipulation.js
@@ -1,19 +1,15 @@
 //getting and adding elements to the page (pre-written code with explanations)
 
-window.onload = function () {
+var getElement = function (classname) {
+  return document.querySelector(classname);
+};
 
-    var getElement = function (classname) {
-        return document.querySelector(classname);
-    };
+var getValue = function (classname) {
+  var el = getElement(classname);
+  return el.innerHTML;
+};
 
-    var getValue = function (classname) {
-        var el = getElement(classname);
-        return el.innerHTML;
-    };
-
-    var displayValue = function (classname, value) {
-        var el = getElement(classname);
-        return el.innerHTML = value;
-    };
-
+var displayValue = function (classname, value) {
+  var el = getElement(classname);
+  return el.innerHTML = value;
 };
